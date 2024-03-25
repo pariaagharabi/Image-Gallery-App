0c72bec5c5ce9418c8a693bde69fbbfe7ccc24fc
PARIA AGHARABI <pariaagharabi@gmail.com>
2024-03-25 14:29:29 -0700

    Adding useState

--- src/app/components/ImageGalleryApp.jsx
+++ src/app/components/ImageGalleryApp.jsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ImageGalleryApp = () => {
     // Initializing, destructuring and updating States
+    const [defaultList, setDefaultList] = useState([
+        { id: crypto.randomUUID().toString(), photo: "test1", title: "cat", },
+        { id: crypto.randomUUID().toString(), photo: "test2", title: "dog", },
+        { id: crypto.randomUUID().toString(), photo: "test3", title: "bear", },
+    ]);
+    console.log(defaultList);
+
+    const [currentValueInput, setCurrentValueInput] = useState("");
+    console.log(currentValueInput);
+
+    const [searchedTitle, setSearchedTitle] = useState(defaultList);
+    console.log(searchedTitle);
 
 
     // Defining Methods
+    const handleInputChange = (e) => {
+        const currentTitle = e.target.value;
+        setCurrentValueInput(currentTitle);
+    };
+
+    const handleSearchedSubmit = (e) => {
+        e.preventDefault();
+
+        const keyword = currentValueInput.toLowerCase();
+        const filterKeywordsInput = defaultList.filter(filterKeyword => filterKeyword.title.toLowerCase().includes(keyword));
+        setSearchedTitle(filterKeywordsInput);
+    }
+
+
+
     const fetchData = async () => {
         try {
             const dataURL = "";
-            const keyAPI = "";
+            const keyAPI = "XyH7IJIPyf9HzMU93hmm25WDvdQCDPm0S09PAE2uP9DzAgTmrSy6m92p";
 
             const response = await fetch(dataURL);
             if (!response.ok) {
                 throw new Error(response.status);
             }
+
+            const data = await response.json();
+            console.log(data);
+
         } catch (err) {
             console.error("Data Fetching Erro: ", err);
         }
+
     }
+    // fetchData();
 
 
 
@@ -32,11 +65,13 @@ const ImageGalleryApp = () => {
                     </div>
 
                     <div className="search-images-container">
-                        <form className="search-box">
+                        <form className="search-box" onSubmit={handleSearchedSubmit}>
                             <input
                                 type="text"
                                 name="searchImageName"
                                 placeholder="Search Photos..."
+                                value={currentValueInput}
+                                onChange={handleInputChange}
                             />
                             <button>Search</button>
                         </form>
@@ -47,38 +82,38 @@ const ImageGalleryApp = () => {
                     </div>
 
                     <div className="show-all-images">
-                        <section class="card">
+                        <section className="card">
                             <figure>
-                                <img src="image.jpg" alt="Card Image" />
-                                <figcaption class="title">Card Title</figcaption>
+                                <img src="" alt="Card Image" />
+                                <figcaption className="title">Card Title</figcaption>
                             </figure>
                         </section>
 
-                        <section class="card">
+                        <section className="card">
                             <figure>
-                                <img src="image.jpg" alt="Card Image" />
-                                <figcaption class="title">Card Title</figcaption>
+                                <img src="" alt="Card Image" />
+                                <figcaption className="title">Card Title</figcaption>
                             </figure>
                         </section>
 
-                        <section class="card">
+                        <section className="card">
                             <figure>
-                                <img src="image.jpg" alt="Card Image" />
-                                <figcaption class="title">Card Title</figcaption>
+                                <img src="" alt="Card Image" />
+                                <figcaption className="title">Card Title</figcaption>
                             </figure>
                         </section>
 
-                        <section class="card">
+                        <section className="card">
                             <figure>
-                                <img src="image.jpg" alt="Card Image" />
-                                <figcaption class="title">Card Title</figcaption>
+                                <img src="" alt="Card Image" />
+                                <figcaption className="title">Card Title</figcaption>
                             </figure>
                         </section>
 
-                        <section class="card">
+                        <section className="card">
                             <figure>
-                                <img src="image.jpg" alt="Card Image" />
-                                <figcaption class="title">Card Title</figcaption>
+                                <img src="" alt="Card Image" />
+                                <figcaption className="title">Card Title</figcaption>
                             </figure>
                         </section>
                     </div>
